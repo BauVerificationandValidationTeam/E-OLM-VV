208b5eb89d69fd73a70373574bcf64a2ef2d3980
selin keskin <130260570+selinkeskinn@users.noreply.github.com>
2026-01-03 21:23:58 +0300

    Update tests.py

--- library/tests.py
+++ library/tests.py
@@ -1,8 +1,10 @@
 from django.test import TestCase
 
-class SmokeTests(TestCase):
+class TestSmokeTests(TestCase):
+
     def test_admin_login_page_loads(self):
         resp = self.client.get("/admin/login/")
         self.assertEqual(resp.status_code, 200)
+
 
 
