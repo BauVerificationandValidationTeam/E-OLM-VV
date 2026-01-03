d5263891a0f4c3d2ebac7c44a055f5b4f1945f54
selin keskin <130260570+selinkeskinn@users.noreply.github.com>
2026-01-03 17:28:26 +0300

    Add smoke test for admin login (5.2)

--- library/tests.py
+++ library/tests.py
@@ -1,1 +1,8 @@
-# Create your tests here.
+from django.test import TestCase
+
+class SmokeTests(TestCase):
+    def test_admin_login_page_loads(self):
+        resp = self.client.get("/admin/login/")
+        self.assertEqual(resp.status_code, 200)
+
+
